1f0152cbdb10c686b372db6f5aec29b908ce782d
AdryanCourtial <adryan.courtial@ynov.com>
2025-12-12 14:55:05 +0100

    :construction: Ajout du hot reload docker

--- Frontend-React/src/pages/Home/Home.tsx
+++ Frontend-React/src/pages/Home/Home.tsx
@@ -8,15 +8,15 @@ function Home() {
 
           <div className='container-solde-wallet' > 
             <p className='title'>Compte de Adryan COURTIAL</p>
-            <h1> 1654,84 € </h1>
-            <p> Solde à venir : 1565,65 €</p>
+            <h1> 1655,84 € </h1>
+            <p> Solde à venir : 1566,65 €</p>
           </div>
 
         <MainInteractiveContainer>
             <div className='comming-mouvement'>
               <div className='flex-row justify-between align-center'>
                 <h3>mouvement à venir</h3>
-                <p>(-123,00 €)</p>
+                <p>(-125,00 €)</p>
               </div>
             </div>
         </MainInteractiveContainer>
