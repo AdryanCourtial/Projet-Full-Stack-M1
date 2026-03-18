e2cc73543e908d8ac0a4d97c5b26a6ce95c42007
AdryanCourtial <adryan.courtial@ynov.com>
2026-03-18 12:21:05 +0100

    :construction: Ajout du seeding des categorie

--- Backend-Express/src/prisma/seeders/seed.ts
+++ Backend-Express/src/prisma/seeders/seed.ts
@@ -1,9 +1,11 @@
 import { seedCategories } from "./categories.seed";
 
-export default function main() {
-    Promise.all([
+export async function seed() {
+    await Promise.all([
         seedCategories()
     ]).then(() => {
         console.log("FIN DU SEEDING DE LA BDD")
     })
 }
+
+seed().catch(() => console.log("UNE ERREUR EST SURVENUE"))
