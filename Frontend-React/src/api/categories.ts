eddea9137dea146faf11c93989a8bc9674b67840
AdryanCourtial <adryan.courtial@ynov.com>
2026-03-19 13:13:32 +0100

    :construction: changement du workflow

---
+++ Frontend-React/src/api/categories.ts
@@ -0,0 +1,10 @@
+import axiosClient from "../config/axios"
+
+export default function CategoriesRequest() {
+
+    const getCategories = async () => {
+
+        
+        const response = await axiosClient.get("categories")
+    }
+}
